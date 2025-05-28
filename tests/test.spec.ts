23a0a348258e39846237709d1b68bb0eed736418
Sarah Schwartz <58856580+sarahschwartz@users.noreply.github.com>
2025-05-28 17:59:47 -0600

    test

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,20 +1,26 @@
 import { test } from "@playwright/test";
-// import { setupAndRunTest } from "../src/utils/runTest";
+import { setupAndRunTest } from "../src/utils/runTest";
 import {
   existsSync,
-  // readFileSync
+  readFileSync
 } from "fs";
 import { join } from "path";
 
 const tutorialPaths = process.env.TUTORIAL_PATHS;
 const configPath = process.env.CONFIG_PATH;
 const customTimeout = process.env.CUSTOM_TIMEOUT;
+const folderName = process.env.FOLDER_NAME;
 const dirPath = process.env.DIR_PATH;
 
 test(`Testing ${tutorialPaths}`, async ({ page, context }) => {
   if (!tutorialPaths) throw new Error("TUTORIAL_PATHS not set");
   if (!configPath) throw new Error("CONFIG_PATH not set");
+  if (!folderName) throw new Error("FOLDER_NAME not set");
   if (!dirPath) throw new Error("DIR_PATH not set");
+    if (customTimeout) {
+    test.setTimeout(parseInt(customTimeout));
+  }
+
   console.log("tutorials:", tutorialPaths);
   const tutorials = JSON.parse(tutorialPaths);
   console.log("tutorials:", tutorials);
@@ -23,21 +29,24 @@ test(`Testing ${tutorialPaths}`, async ({ page, context }) => {
 
   const filePath = join(dirPath, configPath);
   console.log("filePath:", filePath);
-  const file = existsSync(filePath);
-  console.log("file:", file);
-
-  // const testConfig = JSON.parse(readFileSync(resolve(configPath), "utf8"));
-  // console.log("e2eConfig:", testConfig);
-
-  if (customTimeout) {
-    test.setTimeout(parseInt(customTimeout));
+  const fileExists = existsSync(filePath);
+  console.log("fileExists:", fileExists);
+  if(!fileExists){
+    throw new Error(`Config file not found at ${filePath}`);
   }
 
-  //   await setupAndRunTest(
-  //     page,
-  //     context,
-  //     ["/custom-zk-chain", "/custom-zk-chain/customizing-your-chain"],
-  //     "custom-zk-chain",
-  //     testConfig
-  //   );
+  const testConfig = JSON.parse(readFileSync(filePath, "utf8"));
+  console.log("e2eConfig:", testConfig);
+
+    await setupAndRunTest(
+      page,
+      context,
+      tutorials.paths,
+      folderName,
+      testConfig
+    );
+});
+
+test('Build an ERC20 custom paymaster', async ({ page, context }) => {
+  await setupAndRunTest(page, context, ['/erc20-paymaster'], 'erc20-paymaster');
 });
